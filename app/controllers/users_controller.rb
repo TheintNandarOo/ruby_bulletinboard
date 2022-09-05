b33a4a90cd5a6c30a1fa648d8d94dce91f44e4c9
theintnandaroo <theintnandaroo@cgm-myanmar.com>
2022-09-05 17:00:32 +0630

    add user list and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,46 @@
 class UsersController < ApplicationController
-  #function : users
-  #show all users list
-  #return : @users
-  def index
-    @users = UserService.getAllUserList
-  end
+	#function : users
+	#show all users list
+	#return : @users
+	def index
+		@users = UserService.getAllUserList
+	end
 
-  def search_users
-    @users = User.search(params[:search])
-  end
+	def search_users
+		@users = User.where(deleted_id: nil)
+		if params[:name] && params[:name] != ""
+			@users = @users.where(name: params[:name])
+		end
+		if params[:email] && params[:email] != ""
+			@users = @users.where(email: params[:email])
+		end
+		if params[:address] && params[:address] != ""
+			@users = @users.where(address: params[:address])
+		end
+		if params[:phone] && params[:phone] != ""
+			@users = @users.where(phone: params[:phone])
+		end
+		if (params[:from_date] && params[:from_date] != "" && params[:to_date] && params[:to_date] != "")
+			@users = @users.where('created_at BETWEEN ? AND ?', params[:from_date], params[:to_date])
+		end
+	  	render "users/index"
+	end
+
+	def new
+		@user = User.new()
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to users_path
+		else 
+			render :"users/new"
+		end
+	end
+
+	private
+		def user_params
+		params.require(:user).permit(:id, :name, :email, :password, :address, :phone, :dob, :gender, :role, :created_id, :updated_id)
+		end
 end
---
+++ config/initializers/constants.rb
@@ -0,0 +1,10 @@
+module Constants
+    ROLES = {
+        'Admin' => '0',
+        'User' => '1'
+    },
+    TYPES = {
+        'Male' => '0',
+        'Female' => '1'
+    }
+end
